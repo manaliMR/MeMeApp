14e2d889fd618ec2de82282d28ae34661b3e1a12
manali78 <manalirami78@gmail.com>
2018-06-25 21:20:00 -0400

    Initial Commit

---
+++ MemeApp/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  MemeApp
+//
+//  Created by Manali Rami on 2018-06-25.
+//  Copyright © 2018 Manali Rami. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
